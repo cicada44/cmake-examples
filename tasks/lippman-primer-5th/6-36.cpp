b773ca421662956a9b0f35a3fec3f4ab7a228449
Cicada <hubbleadd@bk.ru>
2023-02-14 00:07:28 +0700

    Add few tasks lippman

---
+++ tasks/lippman-primer-5th/6-36.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+#include <string>
+
+std::string arr[10];
+std::string;
+
+// std::string (&func())[10]
+// {
+// }
+
+// auto func() -> std::string (&)[10]
+// {
+// }
+
+// std::string arr[10];
+//
+// std::string (&ref_arr)[10] = arr;
+
+// decltype(ref_arr) func() {
+//
+// }
+
+int main()
+{
+}
