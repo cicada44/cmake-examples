f4cdf6526d7a659a32a37e9cef1d7a8114780a56
Cicada <hubbleadd@bk.ru>
2023-02-14 23:12:18 +0700

    Add overloading func note

---
+++ cpp_notes/basics/functions/overloading.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+void print(const char*);
+void print(const char*, const int*);
+void print();
+
+// cannot overload function by returning value
+double print();
+
+int main()
+{
+}
