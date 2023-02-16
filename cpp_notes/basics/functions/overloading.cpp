011a3180574a2dc0774f523cc9b83fd0b28c3c31
Cicada <hubbleadd@bk.ru>
2023-02-16 23:46:31 +0700

    [CPP_NOTES] Add string to note

--- cpp_notes/basics/functions/overloading.cpp
+++ cpp_notes/basics/functions/overloading.cpp
@@ -2,10 +2,14 @@
 
 void print(const char*);
 void print(const char*, const int*);
+void print(const char*, const int*);
 void print();
 
 // cannot overload function by returning value
 double print();
+
+// ambiguos
+void print(const char*, const int*, int n = 1);
 
 int main()
 {
